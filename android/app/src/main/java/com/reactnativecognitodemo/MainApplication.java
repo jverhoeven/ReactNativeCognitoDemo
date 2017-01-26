cc16e41c37bb5bf751d40bb459df6285d9ca71bf
Jan Verhoeven <jan@visity.nl>
2017-01-26 19:55:40 +0100

    More changes due to move to react-native 4.0.

--- android/app/src/main/java/com/reactnativecognitodemo/MainApplication.java
+++ android/app/src/main/java/com/reactnativecognitodemo/MainApplication.java
@@ -9,7 +9,6 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.reactlibrary.RNCognitoIdentityPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,7 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           // RNCognitoIdentityPackage will be added by react-native link
-          new MainReactPackage());
+          new MainReactPackage()
+      );
     }
   };
 
